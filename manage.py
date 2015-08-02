becb011c656420617d437694e8b29a52d4c2e5fd
Assassi, Youness (ya2257) <ya2257@us.att.com>
2015-08-02 17:53:02 -0400

    Add tags to db with many to many relationship

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from thermos import app, db
-from thermos.models import User, Bookmark
+from thermos.models import User, Bookmark, Tag
 from flask.ext.script import Manager, Server, prompt_bool
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -15,6 +15,27 @@ def insert_users():
 	db.session.add(User(username="assassi", email="assassi@example.com", password="test"))
 	db.session.commit()
 	print 'users added'
+
+
+@manager.command
+def insert_tags():
+	for name in ["python", "flask", "webdev", "programming", "training", "news", "orm", "databases", "emacs", "gtd", "django"]:
+		db.session.add(Tag(name=name))
+	db.session.commit()
+	print 'tags added'
+
+@manager.command
+def insert_bookmarks():
+	youness = User.get_by_username('youness')
+	def add_bookmark(url, description, tags):
+		db.session.add(Bookmark(url=url, description=description, user=youness, tags=tags))
+
+	add_bookmark("http://www.pluralsight.com", "Pluralsight. Hardcore developer training", "training,programming,python,flask,webdev")
+	add_bookmark("http://www.python.org", "Python - my favorite programming language", "pyhton")
+	db.session.commit()
+
+	print 'bookmarks added'
+
 
 # @manager.command
 # def initdb():
