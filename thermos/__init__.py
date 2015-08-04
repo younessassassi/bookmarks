f865b69b20e7761bebe189d72ac80b912b88c54f
Assassi, Youness (ya2257) <ya2257@us.att.com>
2015-08-04 10:35:13 -0400

    add debug toolbar

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask.ext.moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,6 +23,9 @@ login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "login"
 login_manager.init_app(app)
+
+# enable debug toolbar
+toolbar = DebugToolbarExtension(app)
 
 # for displaying timestamps
 moment = Moment(app)
--- thermos/models.py
+++ thermos/models.py
@@ -18,7 +18,7 @@ class Bookmark(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(300))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    _tags = db.relationship('Tag', secondary=tags, 
+    _tags = db.relationship('Tag', secondary=tags, lazy='joined', 
         backref=db.backref('bookmarks', lazy='dynamic')
     )
 
@@ -34,6 +34,8 @@ class Bookmark(db.Model):
     def tags(self, string):
         if string:
             self._tags = [Tag.get_or_create(name) for name in string.split(',')]
+        else:
+            self._tags = []
     
     def __repr__(self):
         return "<Bookmark '{}: '{}'>".format(self.description, self.url)
